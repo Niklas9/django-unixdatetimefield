92794c5f8c6986e7bbfbe09e515974ba71b84514
Niklas Andersson <nandersson900@gmail.com>
2015-03-25 15:58:50 +0000

    replaced built-in assert with nose/django test case standard asserts for better compatibility

--- django_unixdatetimefield/tests/tests.py
+++ django_unixdatetimefield/tests/tests.py
@@ -10,17 +10,17 @@ class UnixDateTimeFieldTestCase(test.TestCase):
     def test_default_field(self):
         d = datetime.datetime(2015, 2, 21, 19, 38, 32, 209148)
         m = tm.DefaultField.objects.create(created_at=d)
-        assert m.created_at == d
+        self.assertEqual(m.created_at, d)
 
     def test_null_field(self):
         m = tm.NullField.objects.create()
-        assert m.created_at == None
+        self.assertEqual(m.created_at, None)
 
     def test_autoset_field(self):
         m = tm.AutosetField.objects.create()
-        assert not m.created_at == None
-        assert not m.updated_at == None
-        assert type(m.created_at) == datetime.datetime
-        assert type(m.updated_at) == datetime.datetime
+        self.assertNotEqual(m.created_at, None)
+        self.assertNotEqual(m.updated_at, None)
+        self.assertTrue(isinstance(m.created_at, datetime.datetime))
+        self.assertTrue(isinstance(m.updated_at, datetime.datetime))
         m.save()
-        assert m.updated_at > m.created_at
+        self.assertGreater(m.updated_at, m.created_at)
