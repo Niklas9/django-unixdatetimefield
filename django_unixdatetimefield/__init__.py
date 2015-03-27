ee81b5260cd1bf039c2a4d96a188617cf4861bdc
Niklas Andersson <nandersson900@gmail.com>
2015-03-27 08:21:15 +0000

    bumped version after the latest changes

--- django_unixdatetimefield/__init__.py
+++ django_unixdatetimefield/__init__.py
@@ -1,1 +1,3 @@
 from .fields import UnixDateTimeField
+
+__version__ = '0.1.3'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='django-unixdatetimefield',
-    version='0.1.2',
+    version='0.1.3',
     author='Niklas Andersson',
     author_email='nandersson900@gmail.com',
     description='UnixDateTimeField in Django',
